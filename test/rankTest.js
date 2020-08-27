695b60f2c9fca2af9239d12cd59cef54cbf57cc6
1764775976@qq.com <1764775976@qq.com>
2020-08-27 19:56:59 +0800

    [homework][richard]test: add test to cover the branches that are not covered

--- test/rankTest.js
+++ test/rankTest.js
@@ -1,10 +1,10 @@
 const {rating} = require('../src/rank')
 const test = require('ava');
 
-test('test should return A when rating given ', t => {
+test('test should return A when rating given voyage with china 19 history length 5', t => {
   //given
   const voyage = {
-    zone: 'china',
+    zone: 'east-indies',
     length: 19,
   };
   const history = [
@@ -17,29 +17,9 @@ test('test should return A when rating given ', t => {
   let result = rating(voyage, history);
   //then
   t.is('B', result);
-
 })
 
-test('test should return B when rating given ', t => {
-  //given
-  const voyage = {
-    zone: 'china',
-    length: 19,
-  };
-  const history = [
-    {
-      zone: 'east-indies',
-      profit: 5,
-    }
-  ];
-  //when
-  let result = rating(voyage, history);
-  //then
-  t.is('B', result);
-
-})
-
-test('test should return B when rating given ', t => {
+test('test should return A when rating given voyage with china 12 and history length 5 ', t => {
   //given
   const voyage = {
     zone: 'china',
@@ -72,6 +52,20 @@ test('test should return B when rating given ', t => {
   //then
   t.is('A', result);
 
+})
+
+test('test should return A when rating given voyage with china 12 and empty history  cover the voyageProfitFactor', t => {
+  //given
+  const voyage = {
+    zone: 'china',
+    length: 12,
+  };
+  const history = [
+  ];
+  //when
+  let result = rating(voyage, history);
+  //then
+  t.is('B', result);
 })
 
 

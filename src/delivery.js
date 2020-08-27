30052c966f22dbc2bd8cc3c5061c2e4f506ccfc0
1764775976@qq.com <1764775976@qq.com>
2020-08-27 23:24:47 +0800

    [homework][richard]refactor : add isRush parameter for calculateDeliveryTime

--- src/delivery.js
+++ src/delivery.js
@@ -1,12 +1,12 @@
 const {getDeliveryTimeModel} = require("../src/deliveryTimeModel")
-function calculateDeliveryTime(anOrder,value) {
-  let deliveryTime = getDeliveryTimeModel(true,anOrder.deliveryState);
+function calculateDeliveryTime(anOrder,value,isRush) {
+  let deliveryTime = getDeliveryTimeModel(isRush,anOrder.deliveryState);
   return anOrder.placedOn.plusDays(value + deliveryTime);
 }
 
 function deliveryDate (anOrder, isRush) {
   if (isRush) {
-    return calculateDeliveryTime(anOrder,1);
+    return calculateDeliveryTime(anOrder,1,isRush);
   }
   else {
     let deliveryTime;
